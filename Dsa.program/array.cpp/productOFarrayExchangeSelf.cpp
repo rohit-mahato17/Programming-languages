72ad4edb2d3510b515d238cfc0269ddab75306d1
Rohit Mahato <rohitmahato88935@gmail.com>
2025-11-20 18:51:17 +0530

    code c++

---
+++ Dsa.program/array.cpp/productOFarrayExchangeSelf.cpp
@@ -0,0 +1,22 @@
+// product of array except self
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+// int main(){
+//     vector<int> nums={1,2,3,4};
+//     vector<int> ans(nums.size());
+
+//     for(int i=0;i<nums.size();i++){
+//         int prod=1;
+//         for(int j=0; j<nums.size(); j++){
+//              if(i != j){
+//                 prod *= nums[j];
+//              }
+//         }
+//         ans[i]= prod;
+//     }
+//     for(int i=0;i<nums.size();i++){
+//         cout<<ans[i]<<" ";
+//     }
+// }
+
